de123740c4d6e011a59d413d5a5fe8cc1102a132
Miranti20 <rantirarrr4505@gmail.com>
2025-05-16 08:25:13 +0700

    Tambah kelas DoubleLinkedList dan fungsi addNode untuk penambahan simpul

--- file1.cpp
+++ file1.cpp
@@ -9,3 +9,42 @@ public:
     Node *next;
     Node *prev;
 };
+
+class DoubleLinkedList
+{
+private:
+     Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        Node *newNode = new Node();
+
+        newNode->noMhs = nim;
+
+        if(START == NULL || nim <= START-> noMhs)
+        {
+            cout << "\nDuplicate number not allowed" << endl;
+            return;
+        }
+
+        newNode-> next = START;
+
+        if (START != NULL)
+            START->prev = newNode;
+
+        newNode->prev = NULL;
+
+        START = newNode;
+        return;
+    }
+};
