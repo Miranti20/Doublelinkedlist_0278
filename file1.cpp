d4100342032ffd81e72dc37b3cb8380799af1be0
Miranti20 <rantirarrr4505@gmail.com>
2025-05-16 09:44:55 +0700

    Lengkapi menu utama dengan opsi tambah,cari, dan keluar

--- file1.cpp
+++ file1.cpp
@@ -198,5 +198,10 @@ int main ()
         cout << "\nMenu:\n";
         cout << "1. Add Record\n";
         cout << "2. Delete Record\n";
+        cout << "3. View Ascending\n";
+        cout << "4. View Descending\n";
+        cout << "5. Search Record\n";
+        cout << "6. exit\n";
+        cout << "Enter your choice: ";
     }
 }
